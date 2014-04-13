b02fb018701735b61eb599f87b0029b214dd8c21
zhouwenhong <zhouwenhong54@gmail.com>
2014-04-13 21:38:35 +0800

    去掉九大优势讲解视频

--- style/js/scripts.js
+++ style/js/scripts.js
@@ -24,8 +24,8 @@ var isMobile = (function(){
 /*-----------------------------------------------------------------------------------*/
 $(document).ready(function(){
     if(!isMobile){
-        var zhaoyong_video = '<embed allowfullscreen="true" allownetworking="internal" allowscriptaccess="never" autostart="true" class="blog_video" enablecontextmenu="False" id="blog_video_1383450259641" invokeurls="false" loop="true" showstatusbar="1" src="http://player.youku.com/player.php/sid/XNTA2NDk1OTAw/v.swf" type="application/x-shockwave-flash" videosource="http://v.youku.com/v_show/id_XNTA2NDk1OTAw.html" videothumb="http://g4.ykimg.com/0100641F46507DF50EF64507E1094B6C194E6D-80F9-0B8E-2CB5-7C9362574B1B" videotitle="赵勇幽默讲解三生中国九大优势%2b%2b" wmode="opaque">';
-        $('#zhaoyong_video').prepend(zhaoyong_video);
+        //var zhaoyong_video = '<embed allowfullscreen="true" allownetworking="internal" allowscriptaccess="never" autostart="true" class="blog_video" enablecontextmenu="False" id="blog_video_1383450259641" invokeurls="false" loop="true" showstatusbar="1" src="http://player.youku.com/player.php/sid/XNTA2NDk1OTAw/v.swf" type="application/x-shockwave-flash" videosource="http://v.youku.com/v_show/id_XNTA2NDk1OTAw.html" videothumb="http://g4.ykimg.com/0100641F46507DF50EF64507E1094B6C194E6D-80F9-0B8E-2CB5-7C9362574B1B" videotitle="赵勇幽默讲解三生中国九大优势%2b%2b" wmode="opaque">';
+        //$('#zhaoyong_video').prepend(zhaoyong_video);
         var baidu_share_string = '<script type="text/javascript" id="bdshare_js" data="type=slide&amp;img=0&amp;pos=right&amp;uid=1354421" ></script><script type="text/javascript" id="bdshell_js"></script><script type="text/javascript">document.getElementById("bdshell_js").src = "http://bdimg.share.baidu.com/static/js/shell_v2.js?cdnversion=" + Math.ceil(new Date()/3600000);</script>';
         $('body').append(baidu_share_string);
     }
